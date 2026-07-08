051bf904bc7edcd9001301a9a55207a793a11d26
span700 <span700@yahoo.com>
2026-07-08 01:50:22 -0400

    Respawn Fixed
    Respawning of ball has been fixed with a box collider GameObject underneath the map with a large box collider size.

--- Yoko's_pinball_unity/Assets/Scripts/Ball.cs
+++ Yoko's_pinball_unity/Assets/Scripts/Ball.cs
@@ -2,13 +2,11 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Ball : MonoBehaviour
 {
     Vector2 previousPosition;
     private new Rigidbody rigidbody;
     private Vector3 startPosition = new Vector3(2.9000001f, -11.0600004f, -3.70000005f);
-
 
     void Start()
     {
@@ -20,7 +18,6 @@ public class Ball : MonoBehaviour
         rigidbody.AddForce(new Vector3(0, 40, 0), ForceMode.VelocityChange);
     }
 
-
     void Update()
     {
         Vector2 position = new Vector2(transform.position.x, transform.position.y);
@@ -28,8 +25,13 @@ public class Ball : MonoBehaviour
         Vector2 rotationAxis = Vector2.Perpendicular(speed);
         transform.Rotate(new Vector3(rotationAxis.x, rotationAxis.y, 0), -speed.magnitude * 40f, Space.World);
         previousPosition = position;
-        if (transform.position.y < -15)
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Deadzone"))
         {
+            Debug.Log("Ball hit Deadzone, resetting.");
             ResetBall();
         }
     }
@@ -38,7 +40,6 @@ public class Ball : MonoBehaviour
     {
         rigidbody.velocity = Vector3.zero;
         rigidbody.angularVelocity = Vector3.zero;
-        transform.position = startPosition;
+        transform.localPosition = startPosition;
     }
-
 }
