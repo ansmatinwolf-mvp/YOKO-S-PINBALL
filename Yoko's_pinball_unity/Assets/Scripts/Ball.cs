87a4dfef795dd7a0e04611cc063a7461b56ee38f
span700 <span700@yahoo.com>
2026-07-08 01:10:12 -0400

    Flippers
    Flippers function now. Still need to figure out why it won't respawn.

--- Yoko's_pinball_unity/Assets/Scripts/Ball.cs
+++ Yoko's_pinball_unity/Assets/Scripts/Ball.cs
@@ -9,12 +9,7 @@ public class Ball : MonoBehaviour
     private new Rigidbody rigidbody;
     private Vector3 startPosition = new Vector3(2.9000001f, -11.0600004f, -3.70000005f);
 
-    private static Vector3 Vector3(double v1, double v2, double v3)
-    {
-        throw new NotImplementedException();
-    }
 
-    // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
@@ -25,7 +20,7 @@ public class Ball : MonoBehaviour
         rigidbody.AddForce(new Vector3(0, 40, 0), ForceMode.VelocityChange);
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         Vector2 position = new Vector2(transform.position.x, transform.position.y);
@@ -33,10 +28,17 @@ public class Ball : MonoBehaviour
         Vector2 rotationAxis = Vector2.Perpendicular(speed);
         transform.Rotate(new Vector3(rotationAxis.x, rotationAxis.y, 0), -speed.magnitude * 40f, Space.World);
         previousPosition = position;
-
-        if (transform.position.y < -30)
+        if (transform.position.y < -15)
         {
-            transform.position = startPosition;
+            ResetBall();
         }
     }
+
+    void ResetBall()
+    {
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.angularVelocity = Vector3.zero;
+        transform.position = startPosition;
+    }
+
 }
